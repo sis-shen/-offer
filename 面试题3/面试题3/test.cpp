223f1843dd86825f91664253452127fb98256d47
sis-shen <adongfa@gmail.com>
2024-01-25 22:21:14 +0800

    233

---
+++ 面试题3/面试题3/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findRepeatDocument(vector<int>& documents) {
+        for (int i = 0; i < documents.size(); i++)
+        {
+
+        }
+    }
+};
+
+
+int main()
+{
+	return 0;
+}
